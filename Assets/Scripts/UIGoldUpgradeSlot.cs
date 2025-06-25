f7a06b5dedaa30a5ba207fcda28846b188a7714b
DESKTOP-NK6CLEO\kag24 <kag2479@naver.com>
2025-06-25 18:12:19 +0900

    골드 업그레이드 비용 표시

--- Assets/Scripts/UIGoldUpgradeSlot.cs
+++ Assets/Scripts/UIGoldUpgradeSlot.cs
@@ -9,6 +9,7 @@ public class UIGoldUpgradeSlot : MonoBehaviour
     [SerializeField] private TextMeshProUGUI upgradeNameText;
     [SerializeField] private TextMeshProUGUI upgradeValueText;
     [SerializeField] private TextMeshProUGUI upgradeMaxText;
+    [SerializeField] private TextMeshProUGUI priceText;
     [SerializeField] private Button addButton;
 
     private PlayerStats stats;
@@ -25,9 +26,13 @@ public class UIGoldUpgradeSlot : MonoBehaviour
 
     public void Refresh()
     {
+        int upgradeValue = stats.GetUpgradeLevel(upgradeType);
+        float price = DataManager.Instance.GetGoldUpgradeData(upgradeType).Price + (DataManager.Instance.GetGoldUpgradeData(upgradeType).PriceIncrease * upgradeValue);
+
         upgradeNameText.text = DataManager.Instance.GetLocalizedText(DataManager.Instance.GetGoldUpgradeData(upgradeType).NameKey);
-        upgradeValueText.text = $"Lv. {stats.GetUpgradeLevel(upgradeType)}";
+        upgradeValueText.text = $"Lv. {upgradeValue}";
         upgradeMaxText.text = $"Max Lv. {stats.GetMaxUpgradeLevel(upgradeType)}";
+        priceText.text = $"{price:F1}";
     }
 
     private void OnClickAdd()
