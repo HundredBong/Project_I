591e60e4f98bd8634ea563a207f5d9a4409da3a5
DESKTOP-NK6CLEO\kag24 <kag2479@naver.com>
2025-07-31 15:11:54 +0900

    상점용 프리팹 제작

--- Assets/Scripts/UIGeneralShopPanel.cs
+++ Assets/Scripts/UIGeneralShopPanel.cs
@@ -7,15 +7,28 @@ public class UIGeneralShopPanel : MonoBehaviour
     [SerializeField] private GameObject _contentPrefab;
     [SerializeField] private RectTransform _contentRoot;
 
+    private List<UIGeneralShopSlot> _slots;
+
+    private void OnEnable()
+    {
+        //LanguageManager.OnLanguageChanged += 
+    }
+
+    private void OnDisable()
+    {
+        //LanguageManager.OnLanguageChanged -=
+    }
+
     private void Start()
     {
         List<GeneralShopData> datas = DataManager.Instance.GetAllGeneralShopDatas();
 
         for (int i = 0; i < datas.Count; i++)
         {
-            //contentPrefab을 root아래에 생성
-            //GetComponent로 가져오고
-            //datas[i]로 초기화해줌
+            GameObject obj = Instantiate(_contentPrefab, _contentRoot);
+            UIGeneralShopSlot slot = obj.GetComponent<UIGeneralShopSlot>();
+            slot.Init(datas[i]);
+            _slots.Add(slot);
         }
     }
 }
--- Assets/Scripts/UIGeneralShopSlot.cs
+++ Assets/Scripts/UIGeneralShopSlot.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class UIGeneralShopSlot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI itemNameText;
+    [SerializeField] private TextMeshProUGUI itemLimitText;
+    [SerializeField] private TextMeshProUGUI itemPriceText;
+    [SerializeField] private TextMeshProUGUI itemSoldOutText;
+
+    [Space(20)]
+    [SerializeField] private Image itemIcon;
+    [SerializeField] private Image priceIconImage;
+
+    private GeneralShopData _data;
+
+    private void OnEnable()
     {
-        
+        LanguageManager.OnLanguageChanged += SetLocalizedText;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        LanguageManager.OnLanguageChanged -= SetLocalizedText;
+    }
+
+    public void Init(GeneralShopData data)
+    {
+        _data = data;
+
+        itemIcon.sprite = DataManager.Instance.GetSpriteByKey(data.IconKey);
+
+        switch (data.PriceType)
+        {
+            case ShopPriceType.Diamond:
+                priceIconImage.sprite = DataManager.Instance.GetSpriteByKey(data.IconKey);
+                break;
+            case ShopPriceType.SkillGem:
+                priceIconImage.sprite = DataManager.Instance.GetSpriteByKey(data.IconKey);
+                break;
+            case ShopPriceType.Cash:
+                priceIconImage.sprite = DataManager.Instance.GetSpriteByKey(data.IconKey);
+                break;
+            case ShopPriceType.Ad:
+                priceIconImage.sprite = DataManager.Instance.GetSpriteByKey(data.IconKey);
+                break;
+            default:
+                priceIconImage.sprite = DataManager.Instance.GetSpriteByKey(data.IconKey);
+                break;
+        }
+
+        SetLocalizedText();
+    }
+
+    private void SetLocalizedText()
+    {
+        itemNameText.text = $"{DataManager.Instance.GetLocalizedText(_data.NameKey)}";
+        itemLimitText.text = $"? / {_data.PurchaseLimit}"; //currentLimitCount같은거 필요함
+        itemPriceText.text = _data.PurchaseLimit.ToString();
+        itemSoldOutText.text = $"{DataManager.Instance.GetLocalizedText($"Shop_SoldOut")}";
     }
 }
